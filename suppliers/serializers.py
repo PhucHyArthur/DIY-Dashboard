90ade37f9a7d860532cb6d8e5fff191a011336d5
Hankrezu <hankrezu@gmail.com>
2024-11-26 18:47:53 +0700

    crud suppliers

--- suppliers/serializers.py
+++ suppliers/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from .models import Representative, Suppliers
+from .models import Suppliers, Representative
 
 class RepresentativeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Representative
-        fields = ['id', 'avatar', 'name', 'birth', 'gender', 'tel', 'email', 'position', 'bank_name', 'bank_branch', 'bank_number', 'swift_code', 'created_at']
+        fields = ['id', 'avatar', 'name', 'birth', 'gender', 'tel', 'email', 'position', 'bank_name', 'bank_branch', 'bank_number', 'swift_code']
 
 class SuppliersSerializer(serializers.ModelSerializer):
     representative = RepresentativeSerializer()
@@ -12,3 +12,23 @@ class SuppliersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Suppliers
         fields = ['id', 'representative', 'avatar', 'name', 'address', 'tel', 'email', 'tax_code', 'bank_name', 'bank_branch', 'bank_number', 'swift_code', 'created_at']
+
+    def update(self, instance, validated_data):
+        # Lấy và xử lý dữ liệu cho representative
+        representative_data = validated_data.pop('representative', None)
+
+        # Cập nhật các trường của Suppliers
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        # Nếu có thông tin representative, cập nhật hoặc tạo mới
+        if representative_data:
+            representative = instance.representative
+            representative_serializer = RepresentativeSerializer(representative, data=representative_data)
+            if representative_serializer.is_valid():
+                representative_serializer.save()  # Cập nhật representative
+            else:
+                raise serializers.ValidationError(representative_serializer.errors)
+
+        instance.save()
+        return instance
--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SuppliersCreateView, SuppliersListView, SuppliersDetailView
+from .views import SuppliersCreateView, SuppliersListView, SuppliersDetailView, SuppliersUpdateView, SuppliersDeleteView
 
 urlpatterns = [
-    path('list/', SuppliersListView.as_view(), name='suppliers-list'),
+    path('', SuppliersListView.as_view(), name='suppliers-list'),
     path('create/', SuppliersCreateView.as_view(), name='suppliers-create'),
     path('detail/<int:pk>/', SuppliersDetailView.as_view(), name='suppliers-detail'),
+    path('update/<int:pk>/', SuppliersUpdateView.as_view(), name='suppliers-update'),
+    path('delete/<int:pk>/', SuppliersDeleteView.as_view(), name='suppliers-delete'),
 ]
