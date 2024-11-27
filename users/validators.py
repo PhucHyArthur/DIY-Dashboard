5cdacce739655e25ca989619ca7ad1aaab7a97e9
Ho Phuc Hy <hophuchynk@gmail.com>
2024-11-27 20:27:11 +0700

    Return id + username

--- users/validators.py
+++ users/validators.py
@@ -3,6 +3,10 @@ from oauth2_provider.models import AccessToken
 
 class CustomOAuth2Validator(OAuth2Validator):
     def save_bearer_token(self, token, request, *args, **kwargs):
+        if request.user:
+            token['username'] = request.user.username
+            token['user_id'] = request.user.id
+
         if request.user and hasattr(request.user, 'role') and request.user.role:
             role_scopes = request.user.role.get_scopes()
             token['scope'] = " ".join(role_scopes)
