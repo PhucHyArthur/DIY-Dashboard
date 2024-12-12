c50707b5872ff7957e938922ba6c8e356fe9ee95
huyenho712002 <hohuyen712002@gmail.com>
2024-12-12 17:24:54 +0700

    fix update

--- orders/models.py
+++ orders/models.py
@@ -23,7 +23,7 @@ class SalesOrder(models.Model):
     def __str__(self):
         return f"Order {self.order_number} - Customer: {self.client}"
 
-
+ 
 class SalesOrderLine(models.Model):
     id = models.AutoField(primary_key=True)
     sales_order = models.ForeignKey(SalesOrder, on_delete=models.CASCADE, related_name='order_lines')
